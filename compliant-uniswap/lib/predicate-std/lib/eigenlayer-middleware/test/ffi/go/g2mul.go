ea27b099454505a6307ff1f75f025d6ab27a085a
Robert Leifke <r.leifke@gmail.com>
2025-02-03 03:27:24 -0500

    chore: added dependencies

--- compliant-uniswap/lib/predicate-std/lib/eigenlayer-middleware/test/ffi/go/g2mul.go
+++ compliant-uniswap/lib/predicate-std/lib/eigenlayer-middleware/test/ffi/go/g2mul.go
@@ -2,29 +2,28 @@ package main
 
 import (
 	"fmt"
-	"math/big"
 	"os"
 	"strings"
-
+	"math/big"
 	"github.com/consensys/gnark-crypto/ecc/bn254"
 )
 
 func main() {
-	// parse args
+	//parse args
 	arg1 := os.Args[1]
 	n := new(big.Int)
 	n, _ = n.SetString(arg1, 10)
 
-	// g2 mul
+	//g2 mul
 	pubkey := new(bn254.G2Affine).ScalarMultiplication(GetG2Generator(), n)
 	px := pubkey.X.String()
 	py := pubkey.Y.String()
 
-	// parse out point coords to big ints
-	pxs := strings.Split(px, "+")
+	//parse out point coords to big ints
+	pxs := strings.Split(px, "+")	
 	pxss := strings.Split(pxs[1], "*")
 
-	pys := strings.Split(py, "+")
+	pys := strings.Split(py, "+")	
 	pyss := strings.Split(pys[1], "*")
 
 	pxsInt := new(big.Int)
@@ -39,7 +38,7 @@ func main() {
 	pyssInt := new(big.Int)
 	pyssInt, _ = pyssInt.SetString(pyss[0], 10)
 
-	// swtich to print coord requested
+	//swtich to print coord requested
 	switch os.Args[2] {
 	case "1":
 		fmt.Printf("0x%064X", pxsInt)
@@ -50,13 +49,14 @@ func main() {
 	case "4":
 		fmt.Printf("0x%064X", pyssInt)
 	}
+
 }
 
 func GetG2Generator() *bn254.G2Affine {
-	g2Gen := new(bn254.G2Affine)
-	g2Gen.X.SetString("10857046999023057135944570762232829481370756359578518086990519993285655852781",
-		"11559732032986387107991004021392285783925812861821192530917403151452391805634")
-	g2Gen.Y.SetString("8495653923123431417604973247489272438418190587263600148770280649306958101930",
-		"4082367875863433681332203403145435568316851327593401208105741076214120093531")
-	return g2Gen
+    g2Gen := new(bn254.G2Affine)
+    g2Gen.X.SetString("10857046999023057135944570762232829481370756359578518086990519993285655852781",
+        "11559732032986387107991004021392285783925812861821192530917403151452391805634")
+    g2Gen.Y.SetString("8495653923123431417604973247489272438418190587263600148770280649306958101930",
+        "4082367875863433681332203403145435568316851327593401208105741076214120093531")
+    return g2Gen
 }
